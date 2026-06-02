00bab4c3b1fb703a7db2620f251158748cb72a17
QCXINT <raflianwari0202@gmail.com>
2026-06-03 05:59:59 +0700

    Refactor: Exclude large PDF assets from serverless function bundling

--- website/next.config.js
+++ website/next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   outputFileTracingRoot: __dirname,
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': ['public/kosakata-kanji/**/*'],
+    },
+  },
 }
 
 module.exports = nextConfig
